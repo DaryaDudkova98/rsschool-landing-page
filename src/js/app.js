e8f9bb2d32e16c087d962b0ee78d67b598d11646
DaryaDudkova98 <dudkovadaryadmitrievna@mail.ru>
2026-09-26 16:09:24 +0300

    feat: add functionality carousel

--- src/js/app.js
+++ src/js/app.js
@@ -1,11 +1,13 @@
 import { initTheme } from './theme.js';
 import { initGallery } from './gallery.js';
 import { initBurger } from './burger.js';
+import { initCarousel } from './carousel.js';
 
 export function initApp() {
     initTheme();
     initGallery();
     initBurger();
+    initCarousel();
 }
 
 initApp();
