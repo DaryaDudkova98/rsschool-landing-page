b740de3a3d3a3cbdadb18b85314c5747fd2ca0a8
DaryaDudkova98 <dudkovadaryadmitrievna@mail.ru>
2026-09-26 19:30:11 +0300

    feat: add functionality modal window for prices

--- src/js/app.js
+++ src/js/app.js
@@ -2,12 +2,14 @@ import { initTheme } from './theme.js';
 import { initGallery } from './gallery.js';
 import { initBurger } from './burger.js';
 import { initCarousel } from './carousel.js';
+import { initModalPrices } from './modal-prices.js';
 
 export function initApp() {
     initTheme();
     initGallery();
     initBurger();
     initCarousel();
+    initModalPrices();
 }
 
 initApp();
